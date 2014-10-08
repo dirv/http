558cf90626103d428f76d97caa997f8beebb6c37
Daniel Irvine <dirvine@8thlight.com>
2014-10-08 19:17:50 +0100

    extract ErrorResource class

--- src/main/java/com/danielirvine/http/NotFoundResource.java
+++ src/main/java/com/danielirvine/http/NotFoundResource.java
@@ -1,16 +1,8 @@
 package com.danielirvine.http;
 
-import static java.util.Arrays.*;
+class NotFoundResource extends ErrorResource {
 
-class NotFoundResource implements Resource {
-
-  public Response toResponse() {
-    return new Response(ResponseCode.NOT_FOUND,
-        new PlainTextHeadedContent(
-          asList(new StringContent("404 Not Found"))));
-  }
-
-  public Resource applyRange(RangeHeader range) {
-    return this;
+  public NotFoundResource() {
+    super(ResponseCode.NOT_FOUND);
   }
 }
--- src/main/java/com/danielirvine/http/UnauthorizedResource.java
+++ src/main/java/com/danielirvine/http/UnauthorizedResource.java
@@ -1,23 +1,10 @@
 package com.danielirvine.http;
 
-import static java.util.Arrays.*;
-
-class UnauthorizedResource implements Resource {
-
-  private final String realm;
+class UnauthorizedResource extends ErrorResource {
 
   public UnauthorizedResource(String realm) {
-    this.realm = realm;
-  }
-
-  public Response toResponse() {
-    return new Response(ResponseCode.UNAUTHORIZED,
-        new HeadedContent(
-          asList(new ResponseHeader("WWW-Authenticate", "Basic realm=\"" + realm + "\"")),
-          asList()));
-  }
-
-  public Resource applyRange(RangeHeader range) {
-    return this;
+    super(ResponseCode.UNAUTHORIZED,
+        new ResponseHeader("WWW-Authenticate",
+          "Basic realm=\"" + realm + "\""));
   }
 }
